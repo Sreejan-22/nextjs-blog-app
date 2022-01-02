c6cb2af8e0947449a47c966818b6e6d6b2f6acf8
Sreejan <sreejan147@gmail.com>
2022-01-02 17:49:26 +0530

    add links to home page, modified title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Sreejan";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "BlogX";
 
 export default function Layout({ children, home }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
@@ -11,9 +12,18 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hi! I am Sreejan. I am a frontend developer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you'll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <br />
+        <h1>Links</h1>
+        <Link href="/posts/first-post">
+          <a>First Post</a>
+        </Link>
+        &nbsp;&nbsp;
+        <Link href="/about">
+          <a>About Page</a>
+        </Link>
       </section>
     </Layout>
   );
