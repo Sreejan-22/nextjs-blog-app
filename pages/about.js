f15cba209aab56b55a32817db7f53bc087197511
Sreejan <sreejan147@gmail.com>
2022-01-02 14:57:33 +0530

    style: add styles to about page using tailwind

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,9 @@
 export default function About() {
-  return <h1>This is the About page</h1>;
+  return (
+    <div className="text-center mt-4">
+      <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
+      <h1 className="text-xl">This is the About page</h1>
+      <h1 className="text-xl">This page has been styled using Tailwind</h1>
+    </div>
+  );
 }
